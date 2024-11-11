80361b1267dcf9f47ccbddd6164b0a254f95fd2c
JTeam1 <julienguyot42@gmail.com>
2024-11-11 12:22:04 +0100

    feat : add some classes and close #5

---
+++ src/main/java/cyu/schoolmanager/Pathway.java
@@ -0,0 +1,13 @@
+package cyu.schoolmanager;
+
+public class Pathway extends StudentGroup {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/cyu/schoolmanager/StudentGroup.java
@@ -0,0 +1,4 @@
+package cyu.schoolmanager;
+
+public abstract class StudentGroup extends Emailable {
+}
