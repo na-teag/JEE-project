8a0203cb3eeb12d6d2dc5c21b9eaefc5fa90a1d5
na-teag <104356632+na-teag@users.noreply.github.com>
2024-11-14 19:01:21 +0100

    feat: remove deploy test

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-rm -r target
-mvn install tomcat7:deploy
-sudo rm -r /var/lib/tomcat9/webapps/ent.war /var/lib/tomcat9/webapps/ent
-sudo cp -r target/projet_jee-1.0-SNAPSHOT /var/lib/tomcat9/webapps/ent
-sudo cp target/projet_jee-1.0-SNAPSHOT.war /var/lib/tomcat9/webapps/ent.war
