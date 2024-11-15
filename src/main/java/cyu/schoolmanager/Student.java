f330726b7f2789002d935089823e9f4dd0d6a30c
na-teag <104356632+na-teag@users.noreply.github.com>
2024-11-15 18:53:36 +0100

    feat: remove unused attribute

--- src/main/java/cyu/schoolmanager/Student.java
+++ src/main/java/cyu/schoolmanager/Student.java
@@ -3,8 +3,6 @@ package cyu.schoolmanager;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
-import java.util.List;
-
 @Entity
 @Table(name = "student")
 public class Student extends Person {
@@ -15,15 +13,6 @@ public class Student extends Person {
 	private Classe classe;
 
 
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(
-			name = "student_course",  // Nom de la table de jointure
-			joinColumns = @JoinColumn(name = "student_id"),  // Clé étrangère vers Student
-			inverseJoinColumns = @JoinColumn(name = "course_id")  // Clé étrangère vers Course
-	)
-	private List<Course> courses;
-
-
 
 	public Classe getClasse() {
 		return classe;
@@ -32,10 +21,4 @@ public class Student extends Person {
 		this.classe = classe;
 	}
 
-	public List<Course> getCourses() {
-		return courses;
-	}
-	public void setCourses(List<Course> courses) {
-		this.courses = courses;
-	}
 }
